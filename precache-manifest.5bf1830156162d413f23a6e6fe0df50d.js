5924216231508bc521261a7b350a62bcfaeb7f6f
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-08-24 10:07:05 -0400

    Updates

---
+++ precache-manifest.5bf1830156162d413f23a6e6fe0df50d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b4222698dce728f093f91210f64744",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e42b268bd0ddaf42eada",
+    "url": "/react-portfolio/static/css/main.af71bd1b.chunk.css"
+  },
+  {
+    "revision": "0191ba4ae67844323134",
+    "url": "/react-portfolio/static/js/2.2e909a11.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.2e909a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e42b268bd0ddaf42eada",
+    "url": "/react-portfolio/static/js/main.b048ba93.chunk.js"
+  },
+  {
+    "revision": "ef573dc301c66cc2bd4c",
+    "url": "/react-portfolio/static/js/runtime-main.476b97d1.js"
+  },
+  {
+    "revision": "e6713b528e00f2b03cc6e25bbb07d5a0",
+    "url": "/react-portfolio/static/media/Hasnain_Khalfan_Resume_2022.e6713b52.pdf"
+  }
+]);
